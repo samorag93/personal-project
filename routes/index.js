d2e3869039ca5e2088d5857436a667f8dc8e26f9
samorag93 <moronyag90@gmail.com>
2025-07-18 23:42:18 +0200

    one more time

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const router = require('express').Router();
 router.use('/', require('./swagger'));
 
 router.get('/', (req, res) => {
+  //#swagger.tags=['API is working']
   res.send('API is working');
 });
 
